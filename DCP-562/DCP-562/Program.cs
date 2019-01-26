8e8b03f8a9bec2ef1e4a72f1caa70a93af75c403
aamir <wasi3aamir@gmail.com>
2019-01-27 00:04:51 +0600

    Accepted

---
+++ DCP-562/DCP-562/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCP_562
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int i = 0; i < T; i++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split(' ');
+                int goal1 = int.Parse(space[0]);
+                int goal2 = int.Parse(space[1]);
+                if (goal1 > goal2)
+                {
+                    Console.WriteLine($"Argentina {goal1} - {goal2} Brazil");
+                }else 
+                {
+                    Console.WriteLine($"Brazil {goal2} - {goal1} Argentina");
+                }
+            }
+        }
+    }
+}
